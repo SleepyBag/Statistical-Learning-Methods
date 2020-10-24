0287e63b842080ec95530d7b303dc3cb43c3809d
Xue Qianming <xueqianming200@gmail.com>
2020-10-24 15:06:33 +0800

    Update: optimized SMO

--- 7.SVM/SVM.py
+++ 7.SVM/SVM.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from rich.console import Console
 from rich.table import Table
 sys.path.append(str(Path(os.path.abspath(__file__)).parent.parent))
-from utils import argmax, argmin, wbline
+from utils import wbline
 
 class SVM:
     def __init__(self, C=1e9, epsilon=1e-6, lr=1e-4, max_steps=1000, verbose=True):
@@ -16,60 +16,92 @@ class SVM:
         self.C = C
         self.epsilon = epsilon
 
-    def _smo_step(self, alpha, score, error, K):
+    def _dual_object(self):
+        return .5 * (self.alpha[:, None] * self.alpha * self.Y[:, None] * self.Y * self.K).sum() - self.alpha.sum()
+
+    def _smo_objective(self, i, j):
+        """
+        The objective function of one step of SMO
+        given the choosed alpha i and alpha j
+        """
+        alpha, Y, K = self.alpha, self.Y, self.K
+        return (alpha[i] * Y[i] * alpha * K[i:] * Y).sum() \
+            + (alpha[j] * Y[j] * alpha * K[j:] * Y).sum() \
+            - .5 * alpha[i] ** 2 * K[i, i] \
+            - .5 * alpha[j] ** 2 * K[j, j] \
+            - alpha[i] * alpha[j] * Y[i] * Y[j] * K[i, j]\
+            - alpha[i] - alpha[j]
+
+    def _smo_step(self):
+        if self.verbose:
+            print('SMO step start...')
+        alpha = self.alpha
+        K = self.K
+        data_size = len(alpha)
+
+        # the prediction of this step
+        pred = (self.alpha * Y * self.K).sum(axis=-1) + self.b
+        # the score of pred
+        score = Y * pred
+        # discrepency between pred and label
+        error = pred - Y
+
+        updated = False
+
         # find the first variable alpha_i
         # which violate KKT constraint
         # first try to find fake support vectors
-        data_size = len(alpha)
-        for i in range(data_size):
-            if abs(0 < alpha[i] < self.C and score[i] - 1) > self.epsilon:
-                break
-        else:
-            # if no fake support vector found, find others
-            for i in range(data_size):
-                # alpha[i] == 0 means i should be classified correctly
-                # alpha[i] == C means i should be classified wrongly
-                if alpha[i] == 0 and score[i] < 1 \
-                   or alpha[i] == self.C and score[i] > 1:
+        # of which 0 < alpha_i < C but score_i isn't 1
+        i_cands = [i for i in range(data_size) if
+                   0 < alpha[i] < self.C and abs(score[i] - 1) > self.epsilon or
+                   alpha[i] == 0 and score[i] < 1 or
+                   alpha[i] == self.C and score[i] > 1]
+        for i in i_cands:
+            # find the second variable
+            # which makes alpha_i change most
+            relative_error = np.abs(error - error[i])
+            j_cands = sorted(list(range(data_size)), key=relative_error.__getitem__)
+            for j in j_cands:
+                if j == i:
+                    continue
+                smo_objective_before = self._smo_objective(i, j)
+
+                # upper bound and lower bound of alpha_j
+                L = max(0, alpha[j] - alpha[i] if Y[i] != Y[j] else alpha[i] + alpha[j] - self.C)
+                H = min(self.C, self.C + alpha[j] - alpha[i] if Y[i] != Y[j] else alpha[i] + alpha[j])
+
+                if self.verbose:
+                    print('SMO chooses: ', i, j)
+                    print("L and H:", L, H)
+                    print('Step begin, current object of dual problem:', smo_objective_before)
+
+                alpha_j_old = alpha[j]
+                eta = K[i, i] + K[j, j] - 2 * K[i, j]
+                # update alpha_j
+                alpha[j] += Y[j] * (error[i] - error[j]) / eta
+                # clip
+                alpha[j] = min(alpha[j], H)
+                alpha[j] = max(alpha[j], L)
+                # update alpha_i
+                alpha[i] += Y[i] * Y[j] * (alpha_j_old - alpha[j])
+                # update b
+                self.b = Y[i] - (alpha * Y * K[i]).sum()
+                if 0 < alpha[j] < self.C:
+                    self.b = (Y[j] - (alpha * Y * K[j]).sum() + self.b) / 2
+                smo_objective_after = self._smo_objective(i, j)
+                if self.verbose:
+                    print('Step end, current object of dual problem:', smo_objective_after)
+                if smo_objective_before - smo_objective_after > self.epsilon:
+                    updated = True
                     break
-            else:
-                return False
-        # find the second variable
-        # which makes alpha_i change most
-        relative_error = np.abs(error - error[i])
-        j = argmax(relative_error)[0]
-        # TODO: don't choose j randomly
-        if j == i:
-            j = (i + 1) % data_size
-        j = i
-        while j == i:
-            j = np.random.randint(0, data_size)
-        print('alpha', alpha)
-        print('score', score)
-        print('error', error)
-        print('relative error', relative_error)
-        print("smo selects ", i, j)
+            if updated:
+                break
+        if self.verbose:
+            print('SMO step end...')
+            print()
+        return len(i_cands) > 0
 
-        alpha_j_old = alpha[j]
-        # upper bound and lower bound of alpha_j
-        L = max(0, alpha[j] - alpha[i] if Y[i] != Y[j] else alpha[i] + alpha[j] - self.C)
-        H = min(self.C, self.C + alpha[j] - alpha[i] if Y[i] != Y[j] else alpha[i] + alpha[j])
-        print("L and H:", L, H)
-        eta = K[i, i] + K[j, j] - 2 * K[i, j]
-        # update alpha_j
-        alpha[j] += Y[j] * (error[i] - error[j]) / eta
-        # clip
-        alpha[j] = min(alpha[j], H)
-        alpha[j] = max(alpha[j], L)
-        # update alpha_i
-        alpha[i] += Y[i] * Y[j] * (alpha_j_old - alpha[j])
-        # update b
-        self.b = Y[i] - (alpha * Y * K[i]).sum()
-        if 0 < alpha[j] < self.C:
-            self.b = (Y[j] - (alpha * Y * K[j]).sum() + self.b) / 2
-        return True
-
-    def fit(self, X, Y, kernel=None):
+    def fit(self, X, Y, kernel=lambda x: x @ x.T):
         """
         optimize SVM with SMO
         X: of shape [data-size, feature-size]
@@ -78,34 +110,18 @@ class SVM:
                 input is X of shape [data-size, feature-size] and
                 output a kernel matrix of shape [data-size, data-size]
         """
+        self.X, self.Y = X, Y
         data_size = len(X)
-        alpha = np.zeros(data_size)
+        self.alpha = np.zeros(data_size)
         self.b = np.random.rand()
 
-        if kernel is None:
-            def kernel(X):
-                return X @ X.T
-
-        K = kernel(X)
+        self.K = kernel(X)
         # optimize
-        while True:
-            # the prediction of this step
-            pred = (alpha * Y * K).sum(axis=-1) + self.b
-            # the score of pred
-            score = Y * pred
-            # discrepency between pred and label
-            error = pred - Y
-
-            # update
-            if not self._smo_step(alpha, score, error, K):
-                break
+        while self._smo_step():
+            pass
 
         # optimized, get w and b
-        self.w = ((alpha * Y)[:, None] * X).sum(axis=0)
-        # find a support vector, because it determines b
-        for i in range(data_size):
-            if 0 < alpha[i] < self.C:
-                break
+        self.w = ((self.alpha * Y)[:, None] * X).sum(axis=0)
         if self.verbose:
             print("Done!")
 
@@ -139,21 +155,15 @@ if __name__ == "__main__":
     Y = np.array([1, -1, -1])
     demonstrate(X, Y, "Example 1")
 
-    # # -------------------------- Example 2 ----------------------------------------
-    # print("Example 2:")
-    # X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    # Y = np.array([1, 1, -1, -1])
-    # demonstrate(X, Y, "Example 2")
-
     # -------------------------- Example 2 ----------------------------------------
+    print("Example 2:")
+    X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    Y = np.array([1, 1, -1, -1])
+    demonstrate(X, Y, "Example 2")
+
+    # -------------------------- Example 3 ----------------------------------------
     print("Example 3:")
     X = np.concatenate((np.random.rand(5, 2), np.random.rand(5, 2) + np.array([1, 1])), axis=0)
     Y = np.array([1, 1, 1, 1, 1, -1, -1, -1, -1, -1])
     print(X, Y)
     demonstrate(X, Y, "Example 3")
-
-    # # -------------------------- Example 2 ----------------------------------------
-    # print("Example 2: SVM cannot solve a simple XOR problem")
-    # X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    # Y = np.array([1, -1, -1, 1])
-    # demonstrate(X, Y, "Example 2: SVM cannot solve a simple XOR problem")
