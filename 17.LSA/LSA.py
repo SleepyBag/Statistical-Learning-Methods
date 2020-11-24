7e46f7ca022ff03953e3035376b3da7e35ebad20
Xue Qianming <xueqianming200@gmail.com>
2020-11-24 20:27:17 +0800

    Update: latent -> topic for LSA

--- 17.LSA/LSA.py
+++ 17.LSA/LSA.py
@@ -10,26 +10,26 @@ def lsa(word_text, k=5, max_iteration=1000):
     given a word-text matrix
     the dimension of the principle component, k
     optimize using the algorithm proposed by Lee and Seung
-    return the word-latent matrix and text-latent matrix
+    return the word-topic matrix and text-topic matrix
     """
     n_word, n_text = word_text.shape
-    word_latent = np.random.rand(n_word, k)
-    latent_text = np.random.rand(k, n_text)
+    word_topic = np.random.rand(n_word, k)
+    topic_text = np.random.rand(k, n_text)
     for i in range(max_iteration):
-        word_latent *= (word_text @ latent_text.T) / (word_latent @ latent_text @ latent_text.T)
-        latent_text *= (word_latent.T @ word_text) / (word_latent.T @ word_latent @ latent_text)
-    return word_latent, latent_text.T
+        word_topic *= (word_text @ topic_text.T) / (word_topic @ topic_text @ topic_text.T)
+        topic_text *= (word_topic.T @ word_text) / (word_topic.T @ word_topic @ topic_text)
+    return word_topic, topic_text.T
 
 if __name__ == '__main__':
     def demonstrate(X, k, desc):
         print(desc)
-        word_latent, text_latent = lsa(X, k=k)
-        print("The latent vectors of all the words are")
-        print(word_latent)
-        print("The latent vectors of all the texts are")
-        print(text_latent)
+        word_topic, text_topic = lsa(X, k=k)
+        print("The topic vectors of all the words are")
+        print(word_topic)
+        print("The topic vectors of all the texts are")
+        print(text_topic)
         print("The recovered word-text matrix is")
-        print(np.round(word_latent @ text_latent.T))
+        print(np.round(word_topic @ text_topic.T))
 
     X = np.array([
         [0, 0, 1, 1, 0, 0, 0, 0, 0],
